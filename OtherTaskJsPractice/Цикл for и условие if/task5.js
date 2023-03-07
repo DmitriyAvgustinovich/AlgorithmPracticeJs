23250ea9bac009d4c2d94d984d7c84eda4118300
Dmitriy <101661452+DmitriyAvgustinovich@users.noreply.github.com>
2023-03-07 19:07:07 +0500

    Add files via upload

---
+++ OtherTaskJsPractice/Цикл for и условие if/task5.js
@@ -0,0 +1,28 @@
+'use sctrict';
+
+// task 5
+
+function getSum() {
+
+    let sumPrime = 0;
+    for (let i = 2; i < 1000; i++) {
+        let isPrime = true;
+
+        for (let j = 2; j < i - 1; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime) {
+            sumPrime += i
+
+        }
+
+    }
+
+    console.log(sumPrime)
+
+}
+
+getSum()
